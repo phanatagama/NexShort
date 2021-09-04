117324bb7138195002959bf444b39cccc87ec46a
phanatagama <phanatagama@gmail.com>
2021-09-05 00:29:51 +0700

    connect using middleware

--- pages/api/utils/db.js
+++ pages/api/utils/db.js
@@ -1,24 +1,28 @@
 import mongoose from 'mongoose';
-// const url = process.env.MONGODB_URL;
-// mongoose.connect(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true
-// });
-const connectDB = handler => async (req, res) => {
-  if (mongoose.connections[0].readyState) {
-    // Use current db connection
-    return handler(req, res);
-  }
-  // Use new db connection
-  await mongoose.connect(process.env.MONGODB_URL, {
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true
-  });
-  return handler(req, res);
-};
+import nextConnect from 'next-connect';
 
-export default connectDB;
+const url = process.env.MONGODB_URL;
+const database = async (req,res,next) => {
+    mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  next();
+}
+// const connectDB = handler => async (req, res) => {
+//   if (mongoose.connections[0].readyState) {
+//     // Use current db connection
+//     return handler(req, res);
+//   }
+//   // Use new db connection
+//   await mongoose.connect(process.env.MONGODB_URL, {
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//     useNewUrlParser: true
+//   });
+//   return handler(req, res);
+// };
+const middleware = nextConnect();
+middleware.use(database);
+export default middleware;
--- pages/api/v1.js
+++ pages/api/v1.js
@@ -1,68 +1,68 @@
 // MongoDB CONNECTION
-// import connectDB from './utils/db';
-import mongoose from 'mongoose';
+import middleware from 'pages/api/utils/db';
+// import mongoose from 'mongoose';
 import nextConnect from 'next-connect';
 
-const connectDB = handler => async (req, res) => {
-    if (mongoose.connections[0].readyState) {
-      // Use current db connection
-      return handler(req, res);
-    }
-    // Use new db connection
-    await mongoose.connect("mongodb+srv://heroku:123@cluster0.bw65i.mongodb.net/shortLink?retryWrites=true&w=majority", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    return handler(req, res);
-  };
-
-function test(req, res) {
-    // console.log('loh');
-    try {
-        // require('pages/api/utils/db');
-        const Shortener = require('pages/api/model/shortener');
-        return res.status(200)
-        .json({
-            status: 'OK',
-            message: 'Berhasil Koneksi mongoDB'
-        });
-    } catch (error) {
-        return res.status(301)
-        .json({
-            status: 'Gagal koneksi mongoDB',
-            message: String(error),
-        })
-    }
-}
-const middleware = nextConnect();
-middleware.use(connectDB(test));
-export default middleware
-// export default function handler(req, res) {
-//     res.send('lol');
-//     if (req.method ==='POST') {
-//         res.send(req.body);
-//         Shortener.insertMany(req.body, (error, result) => {
-//             if (!error) {
-//                 return res.status(201)
-//                 .json({
-//                     status: 'Created',
-//                     message: 'Data has been created',
-//                     data: result,
-//                 })
-//             }
-//             return res.status(304)
-//             .json({
-//                 status: 'Not Modified',
-//                 message: 'Failed to created',
-//                 data: req.body,
-//             })
-//         })
+// const connectDB = handler => async (req, res) => {
+//     if (mongoose.connections[0].readyState) {
+//       // Use current db connection
+//       return handler(req, res);
 //     }
-//     if (req.method ==='GET') {
+//     // Use new db connection
+//     await mongoose.connect("mongodb+srv://heroku:123@cluster0.bw65i.mongodb.net/shortLink?retryWrites=true&w=majority", {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true
+//     });
+//     return handler(req, res);
+//   };
+
+// function test(req, res) {
+//     // console.log('loh');
+//     try {
+//         // require('pages/api/utils/db');
+//         const Shortener = require('pages/api/model/shortener');
 //         return res.status(200)
 //         .json({
 //             status: 'OK',
-//             message: 'Successfully',
+//             message: 'Berhasil Koneksi mongoDB'
 //         });
+//     } catch (error) {
+//         return res.status(301)
+//         .json({
+//             status: 'Gagal koneksi mongoDB',
+//             message: String(error),
+//         })
 //     }
 // }
+
+const handler = nextConnect();
+handler.use(middleware);
+handler.get( (req, res) {
+    if (req.method ==='POST') {
+        res.send(req.body);
+        Shortener.insertMany(req.body, (error, result) => {
+            if (!error) {
+                return res.status(201)
+                .json({
+                    status: 'Created',
+                    message: 'Data has been created',
+                    data: result,
+                })
+            }
+            return res.status(304)
+            .json({
+                status: 'Not Modified',
+                message: 'Failed to created',
+                data: req.body,
+            })
+        })
+    }
+    if (req.method ==='GET') {
+        return res.status(200)
+        .json({
+            status: 'OK',
+            message: 'Successfully',
+        });
+    }
+})
+export default handler
