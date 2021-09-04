d3b0c987083cd5857d7d5bfd3fb65d62f1b5b31d
phanatagama <phanatagama@gmail.com>
2021-09-04 16:47:08 +0700

    fix module not found

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
-import InputBar from '../components/InputBar'
-import TodoState from '../components/context/TodoState'
+import Layout from '../components/Layout.js'
+import InputBar from '../components/InputBar.js'
+import TodoState from '../components/context/TodoState.js'
 
 export default function Home() {
   
