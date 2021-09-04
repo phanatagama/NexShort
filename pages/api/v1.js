5bfa1b02b1a0070d305fe5a1826013772df1da6c
phanatagama <phanatagama@gmail.com>
2021-09-04 22:03:52 +0700

    create dummy endpoint

---
+++ pages/api/v1.js
@@ -0,0 +1,32 @@
+// MongoDB CONNECTION
+require('pages/api/utils/db');
+const Shortener = require('pages/api/model/shortener');
+
+export default function handler(req, res) {
+    if (req.method ==='POST') {
+        res.send(req.body);
+        Shortener.insertMany(req.body, (error, result) => {
+            if (!error) {
+                return res.status(201)
+                .json({
+                    status: 'Created',
+                    message: 'Data has been created',
+                    data: result,
+                })
+            }
+            return res.status(304)
+            .json({
+                status: 'Not Modified',
+                message: 'Failed to created',
+                data: req.body,
+            })
+        })
+    }
+    if (req.method ==='GET') {
+        return res.status(200)
+        .json({
+            status: 'OK',
+            message: 'Successfully',
+        });
+    }
+}
