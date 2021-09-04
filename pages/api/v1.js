f180269a998d47ec883617e2d0d44da4a3519c8c
phanatagama <phanatagama@gmail.com>
2021-09-04 22:19:07 +0700

    create dummy connection

--- pages/api/v1.js
+++ pages/api/v1.js
@@ -1,33 +1,50 @@
 // MongoDB CONNECTION
-require('pages/api/utils/db');
-const Shortener = require('pages/api/model/shortener');
-
-export default function handler(req, res) {
-    res.send('lol');
-    if (req.method ==='POST') {
-        res.send(req.body);
-        Shortener.insertMany(req.body, (error, result) => {
-            if (!error) {
-                return res.status(201)
-                .json({
-                    status: 'Created',
-                    message: 'Data has been created',
-                    data: result,
-                })
-            }
-            return res.status(304)
-            .json({
-                status: 'Not Modified',
-                message: 'Failed to created',
-                data: req.body,
-            })
-        })
-    }
-    if (req.method ==='GET') {
+function test(req, res) {
+    try {
+        require('pages/api/utils/db');
+        const Shortener = require('pages/api/model/shortener');
         return res.status(200)
         .json({
             status: 'OK',
-            message: 'Successfully',
+            message: 'Berhasil Koneksi mongoDB'
         });
+    } catch (error) {
+        return res.status(301)
+        .json({
+            status: 'Gagal koneksi mongoDB',
+            message: String(error),
+        })
     }
 }
+
+// export default function handler(req, res) {
+//     res.send('lol');
+//     if (req.method ==='POST') {
+//         res.send(req.body);
+//         Shortener.insertMany(req.body, (error, result) => {
+//             if (!error) {
+//                 return res.status(201)
+//                 .json({
+//                     status: 'Created',
+//                     message: 'Data has been created',
+//                     data: result,
+//                 })
+//             }
+//             return res.status(304)
+//             .json({
+//                 status: 'Not Modified',
+//                 message: 'Failed to created',
+//                 data: req.body,
+//             })
+//         })
+//     }
+//     if (req.method ==='GET') {
+//         return res.status(200)
+//         .json({
+//             status: 'OK',
+//             message: 'Successfully',
+//         });
+//     }
+// }
+
+exoprt default test;
