22949957876724cc11baa3e5e54099a1baaa2e3e
phanatagama <phanatagama@gmail.com>
2021-09-05 02:21:13 +0700

    delete resultbar

--- components/InputBar.js
+++ components/InputBar.js
@@ -3,12 +3,11 @@ import ResultBar from 'components/ResultBar'
 import TodoContext from 'components/context/TodoContext';
 
 function InputBar() {
-    const { URL, setURL, createURL, isSuccess } = useContext(TodoContext)
+    const { URL, setURL, createURL } = useContext(TodoContext)
     const onCreateShorlink = async (e) => {
         e.preventDefault();
         await createURL(URL);
         console.log('debug 2');
-        console.log(isSuccess);
     }
     return (
         <>
@@ -16,7 +15,7 @@ function InputBar() {
                 <input onChange={ (e) => setURL(e.target.value)} className="my-8 p-2 text-xl font-bold w-4/5 focus:outline-none focus:ring focus:border-blue-300" type="text" placeholder="https://example.com"/>
                 <button className="my-8 p-2 bg-blue-500 rounded w-1/5 text-white transform hover:scale-110 transition ease-in-out duration-700">Short</button>
             </form>
-            {isSuccess && <ResultBar/>}
+            {/* {isSuccess && <ResultBar/>} */}
         </>
     );
 }
