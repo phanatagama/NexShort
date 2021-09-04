092da083dd634df8296c2aca5837283209cea3ab
phanatagama <phanatagama@gmail.com>
2021-09-05 01:54:19 +0700

    edit corr

--- pages/api/v1.js
+++ pages/api/v1.js
@@ -1,30 +1,49 @@
 // MongoDB CONNECTION
 import middleware from 'pages/api/utils/db';
 import nextConnect from 'next-connect';
-const Cors = require('cors');
+const cors = require('cors');
 import Shortener from 'pages/api/model/shortener';
 
-function initMiddleware(middleware) {
-    return (req, res) =>
-      new Promise((resolve, reject) => {
-        middleware(req, res, (result) => {
-          if (result instanceof Error) {
-            return reject(result)
-          }
-          return resolve(result)
-        })
-      })
-  }
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-)
+// function initMiddleware(middleware) {
+//     return (req, res) =>
+//       new Promise((resolve, reject) => {
+//         middleware(req, res, (result) => {
+//           if (result instanceof Error) {
+//             return reject(result)
+//           }
+//           return resolve(result)
+//         })
+//       })
+//   }
+// const cors = initMiddleware(
+//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//   Cors({
+//     // Only allow requests with GET, POST and OPTIONS
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//   })
+// )
 const handler = nextConnect();
 handler.use(middleware);
-// handler.use(cors());
+handler.use(cors());
+handler.post( async (req, res) => {
+  // await cors(req,res)
+  await Shortener.insertMany(req.body, (error, result) => {
+      if (!error) {
+          return res.status(201)
+          .json({
+              status: 'Created',
+              message: 'Data has been created',
+              data: result,
+          })
+      }
+      return res.status(304)
+      .json({
+          status: 'Not Modified',
+          message: 'Failed to created',
+          data: req.body,
+      })
+  })
+})
 handler.get("/:url", (req, res) => {
     Shortener.find({path: req.param.url})
     .then(response => location.href(response.url))
@@ -37,23 +56,5 @@ handler.get("/", (req, res) => {
         message: 'Successfully',
     })
 })
-handler.post( async (req, res) => {
-    await cors(req,res)
-    await Shortener.insertMany(req.body, (error, result) => {
-        if (!error) {
-            return res.status(201)
-            .json({
-                status: 'Created',
-                message: 'Data has been created',
-                data: result,
-            })
-        }
-        return res.status(304)
-        .json({
-            status: 'Not Modified',
-            message: 'Failed to created',
-            data: req.body,
-        })
-    })
-})
+
 export default handler
