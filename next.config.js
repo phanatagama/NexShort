8dcd5d89f66c8ae5532f92008ecd6a8ffc05a735
phanatagama <phanatagama@gmail.com>
2021-09-04 17:07:20 +0700

    setting next config path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, path: false };
 
     return config;
   },
