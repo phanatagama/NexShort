8694d51de1c088b0498d72f18edaf6775666a852
phanatagama <phanatagama@gmail.com>
2021-09-12 22:39:42 +0700

    created API delete method

--- pages/api/[path].js
+++ pages/api/[path].js
@@ -6,7 +6,7 @@ import Shortener from 'pages/api/model/shortener';
 
 const handler = nextConnect({ attachParams: true });
 handler.use(middleware);
-handler.use(cors({methods: ['GET', 'POST', 'OPTIONS'],}));
+handler.use(cors({methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],}));
 handler.post("/api/v1", async (req, res) => {
     await Shortener.insertMany(req.body, (error, result) => {
         if (!error) {
@@ -24,6 +24,26 @@ handler.post("/api/v1", async (req, res) => {
             data: req.body
         });
     })
+  })
+handler.delete("/api/v1/:id", async (req, res) => {
+    await Shortener.deleteOne({_id: req.param('id')})
+    .then((result) =>{
+        if(result.deletedCount > 0){
+          return res.status(200).json({
+                    status: 'Delete',
+                    message: 'Data has been deleted',
+                });
+        }
+        return res.status(304).json({
+            status: 'Not Modified',
+            message: 'Failed to delete',
+        })
+      }).catch((error) => {
+        return res.status(503).json({
+            status: 'Not Modified',
+            message: 'Failed to delete',
+        })
+      });
   })
   
 handler.get('/api/v1', (req, res) => {
