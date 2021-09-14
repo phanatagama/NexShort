8ddc5eb8bf4f3d71fe5673e6a18c518da77c819b
phanatagama <48324618+phanatagama@users.noreply.github.com>
2021-09-15 00:40:48 +0700

    MongoDB URL Connection

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   reactStrictMode: true,
-//   env: {
-//     BASE_URL: "https://nexshort.vercel.app",
-//     MONGODB_URL: "mongodb+srv://heroku:123@cluster0.bw65i.mongodb.net/shortLink?retryWrites=true&w=majority",
-//   },
-//   webpack5: true,
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false, path: false };
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    BASE_URL: "https://nexshort.vercel.app",
+    MONGODB_URL: "mongodb+srv://dicoding:dicoding@cluster0.tl2bq.mongodb.net/shortLink?retryWrites=true&w=majority",
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
 
-//     return config;
-//   },
-// }
+    return config;
+  },
+}
