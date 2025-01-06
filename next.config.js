b850ccad30c1489bc858fa5ea843b5f0949affbf
phanatagama <phanatagama@gmail.com>
2025-01-06 23:06:06 +0700

    feat: change app version

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    // BASE_URL: "https://nexshort.vercel.app",
-    BASE_URL: "http://localhost:3000",
-    API_URL: "http://localhost:3000/api/v1",
+    BASE_URL: "https://nexshort.vercel.app",
+    API_URL: "https://nexshort.vercel.app/api/v1",
+    // BASE_URL: "http://localhost:3000",
+    // API_URL: "http://localhost:3000/api/v1",
     // MONGODB_URL: "mongodb+srv://dicoding:dicoding@cluster0.tl2bq.mongodb.net/shortLink?retryWrites=true&w=majority",
     MONGODB_URL: "mongodb+srv://nexshort:nexshort@cluster0.bw65i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   },
